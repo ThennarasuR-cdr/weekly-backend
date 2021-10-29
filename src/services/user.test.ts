67e481d043b6dd72c959b55d25d0a448e7e9d99e
Thennarasu R <thennarasu.r@thoughtworks.com>
2021-10-29 08:12:19 +0530

    [User] Find user by email

--- src/services/user.test.ts
+++ src/services/user.test.ts
@@ -1,22 +1,36 @@
 import mongoose from 'mongoose';
 
 import User from '../models/user';
-import { findUserById } from './user';
+import { findUserByEmail, findUserById } from './user';
 
 describe('User service', () => {
-	const findUserByIdSpy = jest.spyOn(User, 'findById');
+	const findByIdSpy = jest.spyOn(User, 'findById');
+	const findOneSpy = jest.spyOn(User, 'findOne');
 
-	describe('get User by name', () => {
-		const expectedUser = { id: 'name', name: 'userName', password: 'password' };
-		findUserByIdSpy.mockResolvedValue(expectedUser);
+	describe('find User by id', () => {
+		const expectedUser = { id: 'name', email: 'username@mail.com', password: 'password' };
+		findByIdSpy.mockResolvedValue(expectedUser);
 
-		it('should get the user by id', async () => {
+		it('should find the user by id', async () => {
 			const userId = new mongoose.Types.ObjectId('5d6ede6a0ba62570afcedd3a');
 
 			const actualUser = await findUserById(userId);
 
 			expect(actualUser).toBe(expectedUser);
 			expect(User.findById).toBeCalledWith(userId);
+		});
+	});
+
+	describe('find User by email', () => {
+		const email = 'username@mail.com';
+		const expectedUser = { id: 'name', email, password: 'password' };
+		findOneSpy.mockResolvedValue(expectedUser);
+
+		it('should find the user by email', async () => {
+			const actualUser = await findUserByEmail(email);
+
+			expect(actualUser).toStrictEqual(expectedUser);
+			expect(User.findOne).toBeCalledWith({ email });
 		});
 	});
 });
--- src/services/user.ts
+++ src/services/user.ts
@@ -6,8 +6,13 @@ import { User as UserType } from '../types/user';
 const findUserById = async (id: mongoose.Types.ObjectId): Promise<UserType> => {
 	const user: UserType = await User.findById(id);
 
+	return user;
+};
+
+const findUserByEmail = async (email: string): Promise<UserType> => {
+	const user: UserType = await User.findOne({ email: email });
 
 	return user;
 };
 
-export { findUserById };
+export { findUserById, findUserByEmail };
